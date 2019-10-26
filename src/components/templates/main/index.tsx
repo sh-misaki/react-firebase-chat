ddae0512204f09353b131997edcba2e38a923127
sh-misaki <s.misaki.0000@gmail.com>
2019-10-26 22:01:20 +0900

    sideMenuの作成

--- src/components/templates/main/index.tsx
+++ src/components/templates/main/index.tsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
+
 import styles from './index.module.scss';
+import sideMenu from './sideMenu.json';
 
 type Props = {
 
@@ -9,10 +12,19 @@ class Chat extends Component<Props> {
   render() {
     return (
       <div className={styles.main}>
-        <div className={styles.error}>
-          sideBar
+        <div className={styles.sideMenu}>
+          {
+            sideMenu.menu.map((menu) => (
+              <div className={styles.sideMenuItem}>
+                <i className={classNames("fa", menu.icon)} />
+                <div className={styles.sideMenuItemText}>
+                  {menu.text}
+                </div>
+              </div>
+            ))
+          }
         </div>
-        <div>
+        <div className={styles.container}>
           { this.props.children }
         </div>
       </div>
