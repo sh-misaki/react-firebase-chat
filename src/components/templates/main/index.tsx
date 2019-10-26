6d95a9d80a72e8d5c2e1281d840610122b593c32
sh-misaki <s.misaki.0000@gmail.com>
2019-10-26 22:10:03 +0900

    sideMenuの作成

--- src/components/templates/main/index.tsx
+++ src/components/templates/main/index.tsx
@@ -13,16 +13,24 @@ class Chat extends Component<Props> {
     return (
       <div className={styles.main}>
         <div className={styles.sideMenu}>
-          {
-            sideMenu.menu.map((menu) => (
-              <div className={styles.sideMenuItem}>
-                <i className={classNames("fa", menu.icon)} />
-                <div className={styles.sideMenuItemText}>
-                  {menu.text}
+          <div className={styles.sideMenuUser}>
+            <i className={classNames("fa", "fa-user", styles.sideMenuUserIcon)} />
+            <div className={styles.sideMenuUserName}>
+              Jimi Hendrix
+            </div>
+          </div>
+          <div className={styles.sideMenuList}>
+            {
+              sideMenu.menu.map((menu) => (
+                <div className={styles.sideMenuListItem}>
+                  <i className={classNames("fa", menu.icon)} />
+                  <div className={styles.sideMenuListItemText}>
+                    {menu.text}
+                  </div>
                 </div>
-              </div>
-            ))
-          }
+              ))
+            }
+          </div>
         </div>
         <div className={styles.container}>
           { this.props.children }
