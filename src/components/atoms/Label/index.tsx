40c0edcc07ed4bd7ce420376e77bce773de61934
sh-misaki <s.misaki.0000@gmail.com>
2019-10-28 17:57:45 +0900

    label作成

---
+++ src/components/atoms/Label/index.tsx
@@ -0,0 +1,49 @@
+import React, { Children } from 'react';
+import classNames from 'classnames';
+
+interface Props {
+  className?: string,
+  disabled?: boolean,
+  error?: boolean,
+  focused?: boolean,
+  required?: boolean,
+  text: string,
+}
+
+const Label: React.FunctionComponent<Props> = ({
+  className,
+  children,
+  disabled,
+  error,
+  focused,
+  required,
+  text,
+}) => {
+  return (
+    <label>
+      <div className={classNames({
+        className: className !== "",
+      })}>
+        <span className={classNames({
+          'typography__text--disabled': disabled,
+          'typography__text--error': error,
+          'typography__text--focused': focused,
+        })}>
+          { text }
+        </span>
+        { required ? '必須' : '' }
+      </div>
+      { children }
+    </label>
+  )
+};
+
+Label.defaultProps = {
+  className: "",
+  disabled: false,
+  error: false,
+  focused: false,
+  required: false,
+};
+
+export default Label;
--- src/components/pages/login/index.tsx
+++ src/components/pages/login/index.tsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import MainLayout from 'components/templates/main';
 import Typography from 'components/atoms/Typography';
 import TextField from 'components/atoms/TextField';
+import Label from 'components/atoms/Label';
 
 class Login extends Component {
   render() {
@@ -10,14 +11,22 @@ class Login extends Component {
       <MainLayout>
         <Typography variant="h1">
           Login
+        </Typography>
+        <Label
+          text="名前"
+        >
           <TextField
             name="name"
           />
+        </Label>
+        <Label
+          text="パスワード"
+        >
           <TextField
             name="password"
             type="password"
           />
-        </Typography>
+        </Label>
       </MainLayout>
     )
   }
