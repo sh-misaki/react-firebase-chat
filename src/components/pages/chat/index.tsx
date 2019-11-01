6449dfca8b49710598f6c79316d9a373c9f995d9
sh-misaki <s.misaki.0000@gmail.com>
2019-11-01 20:19:51 +0900

    dom作成

--- src/components/pages/chat/index.tsx
+++ src/components/pages/chat/index.tsx
@@ -1,28 +1,88 @@
 import React, { FunctionComponent } from 'react';
+import classNames from 'classnames';
 
 import MainLayout from 'components/layouts/main';
 import Typography from 'components/atoms/Typography';
-import { TodosState } from 'store/ducks/todos/models';
+import { IOpenChatsState } from 'store/ducks/openChats/models';
+
+import styles from './index.module.scss';
 
 interface IStateProps {
-  todos: TodosState['todos'];
-  visibilityFilter: string;
+  conversations: IOpenChatsState['conversations'];
+  title: IOpenChatsState['title'];
+  members: IOpenChatsState['members'];
 }
 
 interface IDispatchProps {
-  onTodoClick(id: number): void;
-  addTodo(text: string): void;
-  onFilterClick(filter: string): void;
+  
 }
 
 type IProps = IStateProps & IDispatchProps;
 
-const Chat: FunctionComponent<IProps> = () => {
+const Chat: FunctionComponent<IProps> = ({
+  conversations,
+  title,
+  members,
+}) => {
+  console.log(conversations);
+  console.log(title)
+  console.log(members)
   return (
     <MainLayout>
-      <Typography variant="h1">
-        Chat
-      </Typography>
+      <div className={styles.chat}>
+        <div className={styles.layoutMemberList}>
+          <div>
+            <Typography variant="subtitle1">
+              ART WORKS
+            </Typography>
+          </div>
+          <div>
+            <Typography variant="subtitle1">
+              DIRECT MESSAGES
+            </Typography>
+          </div>
+        </div>
+        <div className={styles.layoutConversation}>
+          <Typography
+            variant="h2"
+            className={styles.title}
+          >
+            { title }
+          </Typography>
+          <div className={styles.members}>
+            {
+              members.map((member) => (
+                <div className={styles.membersItem}>
+                  <i className={classNames("fa", "fa-user", styles.conversationsItemIcon)} />
+                </div>
+              ))
+            }
+          </div>
+          <div className={styles.conversations}>
+            {
+              conversations.map((conversation, i) => (
+                <div
+                  key={`conversation_${i}`}
+                  className={styles.conversationsItem}
+                >
+                  <i className={classNames("fa", "fa-user", styles.conversationsItemIcon)} />
+                  <div className={styles.conversationsItemDrawer}>
+                    <div className={styles.conversationsItemDrawerText}>
+                      { conversation.text }
+                    </div>
+                    <div className={styles.conversationsItemDrawerName}>
+                      { conversation.userName }
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+          <div className={styles.input}>
+            Write Something
+          </div>
+        </div>
+      </div>
     </MainLayout>
   )
 }
